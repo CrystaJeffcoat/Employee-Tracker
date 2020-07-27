903c15950c83583257d9850c8b07d84208a8f205
Crysta Jeffcoat <crystajeffcoat@gmail.com>
2020-07-26 20:34:27 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,29 @@
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  port: 3306,
+
+  user: "root",
+
+  password: "",
+  
+  database: "company_db"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+
+  console.log("connected as id " + connection.threadId + "\n");
+  start();
+});
+
+function start() {
+  inquirer
+  .prompt({
+
+  }).then()
+
+};
