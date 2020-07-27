f32ba36c48daef16c90912f897dbb3dda7ceb1d8
Crysta Jeffcoat <crystajeffcoat@gmail.com>
2020-07-26 21:06:50 -0400

    added directory prompts and a function to add an employee

--- server.js
+++ server.js
@@ -15,15 +15,78 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err) {
   if (err) throw err;
-
-  console.log("connected as id " + connection.threadId + "\n");
   start();
 });
 
 function start() {
   inquirer
   .prompt({
+    name: "Directory",
+    type: "list",
+    message: "What would you like to do?",
+    choices: [
+      "View all employees", 
+      "View employee by department", 
+      "View employee by Manager", 
+      "Add employee", 
+      "Remove employee", 
+      "Update employee", 
+      "View all roles",
+      "Exit"
+    ]
+  }).then(function(answer) {
+    switch (answer.Directory) {
+      case "View all employees":
+        
+        break;
+      case "View employee by department":
+        
+        break;
+      case "View employee by Manager":
+        
+        break;
+      case "Add employee":
+        addEmployee();
+        break;
+      case "Remove employee":
+        
+        break;
+      case "Update employee":
+        
+        break;
+      case "View all roles":
+        
+      default:
+        connection.end();
+    };
+  });
+};
 
-  }).then()
-
+function addEmployee() {
+  inquirer
+  .prompt([
+    {
+      name: "first_name",
+      type: "input",
+      message: "What is the employee's first name?"
+    },
+    {
+      name: "last_name",
+      type: "input",
+      message: "What is the employee's last name?"
+    },
+    {
+      name: "employee_role",
+      type: "input",
+      message: "What is the employee's role?"
+    },
+    {
+      name: "employee_manager",
+      type: "input",
+      message: "Who is the employee's manager?"
+    },
+  ])
+  .then(function(answers){
+    // add data to db
+  })
 };
