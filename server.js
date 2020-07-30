9fc5c8c988032bdf065554273bd5fab180173720
Crysta Jeffcoat <crystajeffcoat@gmail.com>
2020-07-29 21:21:45 -0400

    restructured code placement into other files

--- server.js
+++ server.js
@@ -1,23 +1,7 @@
-const mysql = require("mysql");
 const inquirer = require("inquirer");
+const orm = require("./config/orm.js");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-
-  port: 3306,
-
-  user: "root",
-
-  password: "",
-  
-  database: "company_db"
-});
-
-connection.connect(function(err) {
-  if (err) throw err;
-  start();
-});
-
+start();
 function start() {
   inquirer
   .prompt({
@@ -25,68 +9,28 @@ function start() {
     type: "list",
     message: "What would you like to do?",
     choices: [
-      "View all employees", 
-      "View employee by department", 
-      "View employee by Manager", 
-      "Add employee", 
-      "Remove employee", 
-      "Update employee", 
-      "View all roles",
+      "Add",
+      "View",
+      "Update", 
+      "Delete",
       "Exit"
     ]
   }).then(function(answer) {
     switch (answer.Directory) {
-      case "View all employees":
-        
+      case "Add":
+        addData();
         break;
-      case "View employee by department":
-        
+      case "View":
+        viewData();
         break;
-      case "View employee by Manager":
-        
+      case "Update":
+        updateData();
         break;
-      case "Add employee":
-        addEmployee();
+      case "Delete":
+        deleteData();
         break;
-      case "Remove employee":
-        
-        break;
-      case "Update employee":
-        
-        break;
-      case "View all roles":
-        
       default:
         connection.end();
     };
   });
-};
-
-function addEmployee() {
-  inquirer
-  .prompt([
-    {
-      name: "first_name",
-      type: "input",
-      message: "What is the employee's first name?"
-    },
-    {
-      name: "last_name",
-      type: "input",
-      message: "What is the employee's last name?"
-    },
-    {
-      name: "employee_role",
-      type: "input",
-      message: "What is the employee's role?"
-    },
-    {
-      name: "employee_manager",
-      type: "input",
-      message: "Who is the employee's manager?"
-    },
-  ])
-  .then(function(answers){
-    // add data to db
-  })
 };
