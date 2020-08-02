813275236c49059810553ea6b44fe7bfb0649e40
Crysta Jeffcoat <crystajeffcoat@gmail.com>
2020-08-02 19:13:56 -0400

    added a function to add data to sql database

--- config/orm.js
+++ config/orm.js
@@ -1,7 +1,15 @@
 const connection = require("./connection");
 
 const orm = {
-  
+  addData: function(tableName, val) {
+    let queryString = "INSERT INTO ?? SET ?"
+    connection.query(queryString, [tableName, val], function(err, result) {
+      if (err) throw err;
+      console.log(result);
+    });
+  },
 }
+  
+
 
 module.exports = orm;
