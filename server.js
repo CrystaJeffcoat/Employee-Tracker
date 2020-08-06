950f538dd893d5c96d279d1972add1e34a19271c
Crysta Jeffcoat <crystajeffcoat@gmail.com>
2020-08-06 17:00:02 -0400

    This function adds a role and sends data to mysql

--- server.js
+++ server.js
@@ -71,10 +71,62 @@ function addDepartment() {
     name: "dept",
     type: "input",
     message: "Enter department name: "
-  }).then(function(name) {
+  })
+  .then(function(name) {
     orm.addData("Department", {name: name.dept});
   });
 }
+function addRole() {
+  connection.query("SELECT * FROM department", function(err, results){
+    if(err) throw err;
+    inquirer
+    .prompt([
+      {
+        name: "title",
+        type: "input",
+        message: "Enter job title: ",
+      },
+      {
+        name: "salary",
+        type: "input",
+        message: "Enter title salary: ",
+        validate: function(val) {
+          if(isNaN(val) === false) {
+            return true;
+          }
+          return "Please enter a valid number";
+        }
+      },
+      {
+        name: "dept",
+        type: "rawlist",
+        message: "Choose department for this role: ",
+        choices: function() {
+          let choiceArr = [];
+          for (var i = 0; i < results.length; i++) {
+            choiceArr.push(results[i]);
+          };
+          return choiceArr;
+        }
+      }
+    ])
+    .then(async function(answer){
+      let dept;
+      for (i = 0; i < results.length; i++) {
+        if (results[i].name == answer.dept) {
+          dept = results[i].id
+        }
+      }
+      orm.addData("emp_role",
+        {
+          title: answer.title,
+          salary: answer.salary,
+          department_id: await dept
+        }
+      );
+    });
+  });
+};
 
 function viewData() {
 
