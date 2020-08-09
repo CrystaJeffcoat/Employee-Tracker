c877508d1313a476d6378024fa8ba5acc49955a7
Crysta Jeffcoat <crystajeffcoat@gmail.com>
2020-08-09 16:37:33 -0400

    added function to get data from db

--- config/orm.js
+++ config/orm.js
@@ -1,6 +1,17 @@
 const connection = require("./connection");
 
 const orm = {
+  getData: function(tableName, cb) {
+    let queryString = "SELECT * FROM " + tableName 
+    connection.query(queryString, function(err, res) {
+      if (err) throw err;
+      let choiceArr = [];
+      for (var i = 0; i < res.length; i++) {
+        choiceArr.push(res[i]);
+      };
+      cb(choiceArr);
+    });
+  },
   addData: function(tableName, val) {
     let queryString = "INSERT INTO ?? SET ?"
     connection.query(queryString, [tableName, val], function(err, result) {
