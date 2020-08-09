c8caf39791e64cb9a64ad6b4976935f387f30593
Crysta Jeffcoat <crystajeffcoat@gmail.com>
2020-08-09 16:36:04 -0400

    all 'add' functions updated and working

--- server.js
+++ server.js
@@ -76,54 +76,102 @@ function addDepartment() {
     orm.addData("Department", {name: name.dept});
   });
 }
+
 function addRole() {
-  connection.query("SELECT * FROM department", function(err, results){
-    if(err) throw err;
-    inquirer
-    .prompt([
-      {
-        name: "title",
-        type: "input",
-        message: "Enter job title: ",
-      },
-      {
-        name: "salary",
-        type: "input",
-        message: "Enter title salary: ",
-        validate: function(val) {
-          if(isNaN(val) === false) {
-            return true;
-          }
-          return "Please enter a valid number";
+  let dept = [];
+
+  orm.getData("department", function(res){
+    res.forEach(item => dept.push(item));
+  });
+
+  inquirer
+  .prompt([
+    {
+      name: "title",
+      type: "input",
+      message: "Enter job title: ",
+    },
+    {
+      name: "salary",
+      type: "input",
+      message: "Enter title salary: ",
+      validate: function(val) {
+        if(isNaN(val) === false) {
+          return true;
         }
-      },
-      {
-        name: "dept",
-        type: "rawlist",
-        message: "Choose department for this role: ",
-        choices: function() {
-          let choiceArr = [];
-          for (var i = 0; i < results.length; i++) {
-            choiceArr.push(results[i]);
-          };
-          return choiceArr;
-        }
+        return "Please enter a valid number";
       }
-    ])
-    .then(async function(answer){
-      let dept;
-      for (i = 0; i < results.length; i++) {
-        if (results[i].name == answer.dept) {
-          dept = results[i].id
-        }
+    },
+    {
+      name: "dept",
+      type: "rawlist",
+      message: "Choose department for this role: ",
+      choices: dept
+    }
+  ])
+  .then((answer) => {
+    let deptId = dept.find(val => val.name == answer.dept);
+
+    orm.addData("emp_role",
+      {
+        title: answer.title,
+        salary: answer.salary,
+        department_id: deptId.id
       }
-      orm.addData("emp_role",
-        {
-          title: answer.title,
-          salary: answer.salary,
-          department_id: await dept
-        }
-      );
+    );
+  });
+};
+
+function addEmployee() {
+  let managerArr = [];
+  let managerNames = [];
+  let roleArr = [];
+  let roleNames = []; 
+
+  orm.getData("employee", function(res){
+    res.forEach(item => managerArr.push(item));
+    managerArr.forEach(item => managerNames.push(item.first_name + " " + item.last_name));
+  });
+  orm.getData("emp_role", function(res){
+    res.forEach(item => roleArr.push(item));
+    roleArr.forEach(item => roleNames.push(item.title));
+  });
+
+  inquirer.prompt([
+    {
+      name: "first",
+      type: "input",
+      message: "Enter employees first name: "
+    },
+    {
+      name: "last",
+      type: "input",
+      message: "Enter employees last name: "
+    },
+    {
+      name: "role",
+      type: "rawlist",
+      message: "What is the employees role? ",
+      choices: roleNames
+    },
+    {
+      name: "manager",
+      type: "list",
+      message: "Who is the employees manager? ",
+      choices: managerNames
+    }
+  ])
+  .then((answer) => {
+    let roleId = roleArr.find(val => val.title == answer.role);
+    console.log(roleId);
+    let managerId = managerArr.find(val => (val.first_name + " " + val.last_name) == answer.manager);
+    console.log(managerId);
+
+    orm.addData("employee", {
+      first_name: answer.first,
+      last_name: answer.last,
+      role_id: roleId.id,
+      manager_id: managerId.id
     });
   });
 };
