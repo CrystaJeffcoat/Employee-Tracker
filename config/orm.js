58dd0a7efc687282e5307c022abc8816d944d590
Crysta Jeffcoat <crystajeffcoat@gmail.com>
2020-08-10 16:09:18 -0400

    relocated view functions and added and update data function

--- config/orm.js
+++ config/orm.js
@@ -19,59 +19,13 @@ const orm = {
       console.log("Successfully added data!");
     });
   },
-  viewAllEmployees: function(cb) {
-    let queryString = 
-    `SELECT e.first_name, e.last_name, title, salary, department, 
-    concat(m.first_name," ", m.last_name) as manager 
-    FROM employee e
-    left join employee m ON e.manager_id = m.id
-    left JOIN emp_role ON e.role_id = emp_role.id
-    left JOIN department ON emp_role.department_id = department.id`
-    connection.query(queryString,function(err, result) {
+  updateData: function(tableName, val1, val2) {
+    let queryString = "UPDATE ?? SET ? WHERE ?"
+    connection.query(queryString, [tableName, val1, val2], function(err, result) {
       if (err) throw err;
-      cb(result);
-    });
-  },
-  byDepartment: function(cb) {
-    let queryString = 
-    `SELECT department, first_name, last_name 
-    FROM employee e
-    left JOIN emp_role ON e.role_id = emp_role.id
-    left JOIN department ON emp_role.department_id = department.id
-    order by department ASC`
-    connection.query(queryString,function(err, result) {
-      if (err) throw err;
-      cb(result);
-    });
-  },
-  byManager: function(cb) {
-    let queryString = 
-    `SELECT concat(m.first_name," ", m.last_name) as manager,
-    department, e.first_name, e.last_name 
-    FROM employee e
-    join employee m ON e.manager_id = m.id
-    left JOIN emp_role ON e.role_id = emp_role.id
-    left JOIN department ON emp_role.department_id = department.id
-    order by manager ASC`
-    connection.query(queryString,function(err, result) {
-      if (err) throw err;
-      cb(result);
-    });
-  },
-  byBudget: function(cb) {
-    let queryString =
-    `SELECT department, SUM(salary) as "total salaries"
-    FROM department d
-    join emp_role r ON r.department_id = d.id
-    join employee e ON e.role_id = r.id
-    group by department`
-    connection.query(queryString,function(err, result) {
-      if (err) throw err;
-      cb(result);
+      console.log("Successfully updated data!");
     });
   }
 }
-  
-
 
 module.exports = orm;
