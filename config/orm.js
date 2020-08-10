17f2bae6c4ec4df1aceeb874bc271176d6c62bb3
Crysta Jeffcoat <crystajeffcoat@gmail.com>
2020-08-10 13:09:32 -0400

    added queries for viewing functions

--- config/orm.js
+++ config/orm.js
@@ -21,7 +21,7 @@ const orm = {
   },
   viewAllEmployees: function(cb) {
     let queryString = 
-    `SELECT e.first_name, e.last_name, title, salary, (name), 
+    `SELECT e.first_name, e.last_name, title, salary, department, 
     concat(m.first_name," ", m.last_name) as manager 
     FROM employee e
     left join employee m ON e.manager_id = m.id
@@ -29,7 +29,44 @@ const orm = {
     left JOIN department ON emp_role.department_id = department.id`
     connection.query(queryString,function(err, result) {
       if (err) throw err;
-      console.log(result);
+      cb(result);
+    });
+  },
+  byDepartment: function(cb) {
+    let queryString = 
+    `SELECT department, first_name, last_name 
+    FROM employee e
+    left JOIN emp_role ON e.role_id = emp_role.id
+    left JOIN department ON emp_role.department_id = department.id
+    order by department ASC`
+    connection.query(queryString,function(err, result) {
+      if (err) throw err;
+      cb(result);
+    });
+  },
+  byManager: function(cb) {
+    let queryString = 
+    `SELECT concat(m.first_name," ", m.last_name) as manager,
+    department, e.first_name, e.last_name 
+    FROM employee e
+    join employee m ON e.manager_id = m.id
+    left JOIN emp_role ON e.role_id = emp_role.id
+    left JOIN department ON emp_role.department_id = department.id
+    order by manager ASC`
+    connection.query(queryString,function(err, result) {
+      if (err) throw err;
+      cb(result);
+    });
+  },
+  byBudget: function(cb) {
+    let queryString =
+    `SELECT department, SUM(salary) as "total salaries"
+    FROM department d
+    join emp_role r ON r.department_id = d.id
+    join employee e ON e.role_id = r.id
+    group by department`
+    connection.query(queryString,function(err, result) {
+      if (err) throw err;
       cb(result);
     });
   }
