3985a1cca436ebc8f66f8e669855947dcd57ff12
Crysta Jeffcoat <crystajeffcoat@gmail.com>
2020-08-09 22:22:22 -0400

    view all employees query function added

--- config/orm.js
+++ config/orm.js
@@ -16,9 +16,23 @@ const orm = {
     let queryString = "INSERT INTO ?? SET ?"
     connection.query(queryString, [tableName, val], function(err, result) {
       if (err) throw err;
-      console.log(result);
+      console.log("Successfully added data!");
     });
   },
+  viewAllEmployees: function(cb) {
+    let queryString = 
+    `SELECT e.first_name, e.last_name, title, salary, (name), 
+    concat(m.first_name," ", m.last_name) as manager 
+    FROM employee e
+    left join employee m ON e.manager_id = m.id
+    left JOIN emp_role ON e.role_id = emp_role.id
+    left JOIN department ON emp_role.department_id = department.id`
+    connection.query(queryString,function(err, result) {
+      if (err) throw err;
+      console.log(result);
+      cb(result);
+    });
+  }
 }
   
 
