59a2f837171c528ac44c166ee8d5ec02a40b7e57
Crysta Jeffcoat <crystajeffcoat@gmail.com>
2020-08-10 13:12:14 -0400

    removed query

--- db/seeds.sql
+++ db/seeds.sql
@@ -9,6 +9,3 @@ INSERT INTO emp_role (title, salary, department_id) VALUES ("Payroll", 45600, 1)
 INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Andrew", "Boyd", 1, NULL);
 INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Brian", "Till", 2, NULL);
 INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Sue", "Kowl", 2, 1);
-
-SELECT first_name, last_name, title, salary, name, manager FROM employee
-JOIN emp_role ON emp_role.primarykey = employee.role_id
