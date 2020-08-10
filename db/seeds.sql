7011429708ad3ea60c03df22a9b5046304fdef7f
Crysta Jeffcoat <crystajeffcoat@gmail.com>
2020-08-10 13:11:03 -0400

    changed department name to 'department'

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,6 @@
-INSERT INTO department (name) VALUES ("Accounting");
-INSERT INTO department (name) VALUES ("Engineering");
-INSERT INTO department (name) VALUES ("Sales");
+INSERT INTO department (department) VALUES ("Accounting");
+INSERT INTO department (department) VALUES ("Engineering");
+INSERT INTO department (department) VALUES ("Sales");
 
 INSERT INTO emp_role (title, salary, department_id) VALUES ("Manager", 120000, 3);
 INSERT INTO emp_role (title, salary, department_id) VALUES ("Engineer", 43000, 2);
@@ -9,3 +9,6 @@ INSERT INTO emp_role (title, salary, department_id) VALUES ("Payroll", 45600, 1)
 INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Andrew", "Boyd", 1, NULL);
 INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Brian", "Till", 2, NULL);
 INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Sue", "Kowl", 2, 1);
+
+SELECT first_name, last_name, title, salary, name, manager FROM employee
+JOIN emp_role ON emp_role.primarykey = employee.role_id
